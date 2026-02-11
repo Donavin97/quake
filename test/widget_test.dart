640924028d32a73f3094d1b51e2f793e262802fb
donavinliebgott <donavinliebgott@gmail.com>
2026-02-11 10:02:50 +0000

    Fix widget test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,11 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:myapp/main.dart';
+import 'package:myapp/notification_service.dart';
+
+class MockNotificationService implements NotificationService {
+  @override
+  bool get isPermissionGranted => false;
+
+  @override
+  Future<void> init() async {
+    // Mock implementation
+  }
+
+  @override
+  Future<void> checkPermission() async {
+    // Mock implementation
+  }
+
+  @override
+  Future<void> requestPermission() async {
+    // Mock implementation
+  }
+
+  @override
+  void onDidReceiveNotificationResponse(
+      NotificationResponse notificationResponse) {
+    // Mock implementation
+  }
+
+  @override
+  Future<void> showNotification(
+      {required int id,
+      required String title,
+      required String body,
+      required String payload}) async {
+    // Mock implementation
+  }
+}
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(MyApp(notificationService: MockNotificationService()));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
