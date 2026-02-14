58e2ff960179b5df32bafe7ebd5c2a26153541d7
donavinliebgott <donavinliebgott@gmail.com>
2026-02-14 23:10:05 +0000

    Fix: Changed Provider to ChangeNotifierProvider for AuthService

--- lib/main.dart
+++ lib/main.dart
@@ -41,7 +41,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
-        Provider<AuthService>(
+        ChangeNotifierProvider<AuthService>(
           create: (_) => AuthService(),
         ),
         Provider<NotificationService>(
